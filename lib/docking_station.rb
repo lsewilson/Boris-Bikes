4e73fc28a83097b1596a8afd90801a22fe8670aa
Andrea Wildsmith <andreawildsmith@Andreas-MacBook-Pro.local>
2016-08-16 18:45:56 +0100

    docking station unit tests

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,9 +1,11 @@
 require_relative 'bike'
 
 class DockingStation
+
+
   def release_bike
     Bike.new
   end
-  def dock_bike
+  def dock_bike(bike)
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -6,7 +6,7 @@ describe DockingStation do
     bike = subject.release_bike
     expect(bike).to be_working
   end
-  it {is_expected.to respond_to :dock_bike}
+  it {is_expected.to respond_to(:dock_bike).with(1).argument}
 
 
 end
