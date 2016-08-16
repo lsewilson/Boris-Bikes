4b69302ce0c0eb3ffc66c48139ce63705906ad98
Andrea Wildsmith <andreawildsmith@Andreas-MacBook-Pro.local>
2016-08-16 16:17:26 +0100

    Pass 3 unit tests

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,4 +2,9 @@ require 'docking_station.rb'
 
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
+  it 'gets a new bike' do
+    bike = subject.release_bike
+    expect(bike).to be_working
+  end
+
 end
