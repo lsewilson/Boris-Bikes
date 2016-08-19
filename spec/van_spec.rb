25cf280e62ba87d60c5638871b57b6fc7de6cc10
lsewilson <laurawilson@hotmail.co.uk>
2016-08-19 15:03:43 +0100

    refactors van spec to use doubles

--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -2,23 +2,16 @@ require 'van'
 
 describe Van do
 
-  it { is_expected.to respond_to :collect_broken_bikes }
+  let(:broken_bike) { double :broken_bike, report_broken: true, broken: true, working?:false }
+  let(:bike) { double :bike, broken: false, working?: true }
+  let(:station) { double :station, dock_bike: true, bikes: [broken_bike, bike, bike, bike] }
 
   describe '#collect_broken_bikes' do
 
-    # let(:broken_bike) { double :broken_bike, report_broken: true, broken: true, working?:false }
-    # let(:station) { double :station, dock_bike: true, bikes:  }
-
     it 'should collect broken bikes from the docking station' do
-      bike_broken = Bike.new
-      station = DockingStation.new
-      bike_broken.report_broken
-      station.dock_bike(bike_broken)
-      10.times do
-        bike = Bike.new
-        station.dock_bike(bike)
-      end
-      expect(subject.collect_broken_bikes(station)).to eq [bike_broken]
+      broken_bike.report_broken
+      station.dock_bike(broken_bike)
+      expect(subject.collect_broken_bikes(station)).to eq [broken_bike]
     end
 
   end
@@ -26,12 +19,10 @@ describe Van do
   describe '#deliver' do
 
     it 'should deliver all broken bikes in van to garage' do
-        bike = Bike.new
-        station = DockingStation.new
-        bike.report_broken
-        station.dock_bike(bike)
+        broken_bike.report_broken
+        station.dock_bike(broken_bike)
         subject.collect_broken_bikes(station)
-        expect(subject.deliver).to eq [bike] 
+        expect(subject.deliver).to eq [broken_bike]
     end
 
   end
