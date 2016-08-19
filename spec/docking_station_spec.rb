30a52636363329a9e0a68927c223262e123e4c96
lsewilson <laurawilson@hotmail.co.uk>
2016-08-19 11:47:43 +0100

    labels doubles more clearly

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,31 +2,31 @@ require 'docking_station.rb'
 
 describe DockingStation do
 
-  let(:bike) { double :bike , broken: false , working?: true }
-  let(:bike2) { double :bike2, report_broken: true, broken: true, working?: false }
+  let(:working_bike) { double :working_bike , broken: false , working?: true }
+  let(:broken_bike) { double :broken_bike, report_broken: true, broken: true, working?: false }
 
   describe '#release_bike' do
 
     it 'gets a bike from docked bikes' do
-      subject.dock_bike(bike)
-      expect(subject.release_bike).to eq bike
+      subject.dock_bike(working_bike)
+      expect(subject.release_bike).to eq working_bike
     end
 
     it 'releases a working bike' do
-      subject.dock_bike(bike)
+      subject.dock_bike(working_bike)
       expect(subject.release_bike).to be_working
     end
 
     it 'docks two bikes, one working and one broken, and releases the working bike' do
-      subject.dock_bike(bike)
-      bike2.report_broken
-      subject.dock_bike(bike2)
-      expect(subject.release_bike).to be bike
+      subject.dock_bike(working_bike)
+      broken_bike.report_broken
+      subject.dock_bike(broken_bike)
+      expect(subject.release_bike).to be working_bike
     end
 
     it 'raises error when only broken bikes' do
-      bike2.report_broken
-      subject.dock_bike(bike2)
+      broken_bike.report_broken
+      subject.dock_bike(broken_bike)
       expect{subject.release_bike}.to raise_error "There are no bikes available"
     end
 
@@ -39,24 +39,24 @@ describe DockingStation do
   describe '#dock_bike' do
 
     it 'docks a bike' do
-     expect(subject.dock_bike(bike)).to eq [bike]
+     expect(subject.dock_bike(working_bike)).to eq [working_bike]
     end
 
     it 'raises error when trying to dock a bike in a full docking station' do
-      DockingStation::DEFAULT_CAPACITY.times {subject.dock_bike(bike)}
-      expect{subject.dock_bike(bike)}.to raise_error "This docking station is full"
+      DockingStation::DEFAULT_CAPACITY.times {subject.dock_bike(working_bike)}
+      expect{subject.dock_bike(working_bike)}.to raise_error "This docking station is full"
     end
 
     it 'docks any bike even if it is broken' do
-      bike2.report_broken
-      expect(subject.dock_bike(bike2)).to eq [bike2]
+      broken_bike.report_broken
+      expect(subject.dock_bike(broken_bike)).to eq [broken_bike]
     end
 
   end
 
   it 'returns docked bike' do
-    subject.dock_bike(bike)
-    expect(subject.bikes).to eq [bike]
+    subject.dock_bike(working_bike)
+    expect(subject.bikes).to eq [working_bike]
   end
 
   it 'initializes with a capacity argument' do
